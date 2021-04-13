9ac669ab188661463592338c7d5d16959d2f76ce
Roman Rodionov <kpackl14@gmail.com>
2021-04-13 23:42:59 +0700

    Добавьте файлы проекта.

---
+++ Lab_2.1/Calc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Lab_2_1
+{
+    public class Calc
+    {
+        Random rand = new Random();
+        public double val1;
+        public double val2;
+   
+        public Calc()
+        {
+            val1 = rand.Next(0, 10);
+            val2 = rand.Next(0, 10);
+        }
+
+        public string Add()
+        {
+            return "" + val1 + " + " + val2 + " = " + (val1 + val2);
+        }
+
+        public string Sub()
+        {
+            return "" + val1 + " - " + val2 + " = " + (val1 - val2);
+        }
+
+        public string Mult()
+        {
+            return "" + val1 + " * " + val2 + " = " + (val1 * val2);
+        }
+
+        public string Div()
+        {
+            if (val2 != 0)
+                return "" + val1 + " / " + val2 + " = " + Math.Round(val1 / val2);
+            else
+                return "" + val1 + " / " + val2 + " = " + "Error: Division by zero";
+        }
+    }
+}
---
+++ Lab_2.1/Models/CalcModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_2_1.Models
+{
+    public class CalcModel
+    {
+        public double val1 { get; set; }
+        public double val2 { get; set; }
+        public double add { get; set; }
+        public double sub { get; set; }
+        public double mult { get; set; }
+        public string div { get; set; }
+    }
+}
